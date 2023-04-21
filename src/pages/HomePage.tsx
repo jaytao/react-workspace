51c232d7989a27214c83a5fac0bc43532a9f5698
jeffs7tao <jeffs7tao@gmail.com>
2023-04-21 14:54:58 -0400

    init

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/NavBar";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Home Page</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+};
+
+export default HomePage;
